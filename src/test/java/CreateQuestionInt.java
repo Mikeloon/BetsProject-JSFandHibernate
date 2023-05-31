8d38b488846884c383f17e6b2dd898080b051e84
Mikel Lonbide <100244220+Mikeloon@users.noreply.github.com>
2023-05-31 11:40:02 +0200

    Update CreateQuestionInt.java

--- src/test/java/CreateQuestionInt.java
+++ src/test/java/CreateQuestionInt.java
@@ -20,115 +20,40 @@ import exceptions.QuestionAlreadyExist;
 import test.businessLogic.TestFacadeImplementation;
 
 public class CreateQuestionInt {
-	 static BLFacadeImplementation sut;
-	 static TestFacadeImplementation testBL;
+	 private CreateQuestionInt createQuestionInt;
 
-	private Event ev;
-	
-	@BeforeClass
-	public static void setUpClass() {
-		//sut= new BLFacadeImplementation();
-		
-		// you can parametrize the DataAccess used by BLFacadeImplementation
-		//DataAccess da= new DataAccess(ConfigXML.getInstance().getDataBaseOpenMode().equals("initialize"));
-		DataAccess da= new DataAccess();
+@BeforeEach
+public void setUp() {
+    createQuestionInt = new CreateQuestionInt();
+}
 
-		sut=new BLFacadeImplementation(da);
-		
-		testBL= new TestFacadeImplementation();
-	}
-	
-	@Test
-	//sut.createQuestion:  The event has one question with a queryText. 
-	public void test1() {
-		try {
-			
-			//define paramaters
-			String eventText="event1";
-			String queryText="query1";
-			Float betMinimum=new Float(2);
-			
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-			Date oneDate=null;;
-			try {
-				oneDate = sdf.parse("05/10/2022");
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}	
-			
-			//configure the state of the system (create object in the dabatase)
-			ev = testBL.addEventWithQuestion(eventText,oneDate,queryText,betMinimum );
-			
-			
-			//invoke System Under Test (sut)  
-			sut.createQuestion(ev, queryText, betMinimum);
-			
-			
-			//if the program continues fail
-		    fail();
-		   } catch (QuestionAlreadyExist e) {
-			// TODO Auto-generated catch block
-			// if the program goes to this point OK  
-			assertTrue(true);
-			} catch (EventFinished e) {
-				// if the program goes to this point fail
-			    fail();
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-				  //Remove the created objects in the database (cascade removing)   
-		          boolean b=testBL.removeEvent(ev);
-		           System.out.println("Finally "+b);          
-		        }
-		   }
-	
-	@Test
-	//sut.createQuestion:  The event has NOT one question with a queryText. 
-	public void test2() {
-		try {
-			
-			//define paramaters
-			String eventText="event1";
-			String queryText="query1";
-			Float betMinimum=new Float(2);
-			
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-			Date oneDate=null;;
-			try {
-				oneDate = sdf.parse("05/10/2022");
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}	
-			
-			//configure the state of the system (create object in the dabatase)
-			ev = testBL.addEventWithQuestion(eventText,oneDate,"query2",betMinimum );
-			
-			//invoke System Under Test (sut)  
-			Question q=sut.createQuestion(ev, queryText, betMinimum);
-			
-			
-			//verify the results
-			assertTrue(q!=null);
-			assertEquals(q.getQuestion(),queryText);
-			assertEquals(q.getBetMinimum(),betMinimum,0);
-			
-			
-		   } catch (QuestionAlreadyExist e) {
-			// TODO Auto-generated catch block
-			// if the program goes to this point fail  
-			fail();
-			} catch (EventFinished e) {
-				// if the program goes to this point fail
-			    fail();
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-				  //Remove the created objects in the database (cascade removing)   
-		          boolean b=testBL.removeEvent(ev);
-		           System.out.println("Finally "+b);          
-		        }
-		   }
+@Test
+public void testCalculateMaxPoints() {
+    assertEquals(10, createQuestionInt.calculateMaxPoints(5, 2));
+    assertEquals(0, createQuestionInt.calculateMaxPoints(0, 0));
+    assertEquals(15, createQuestionInt.calculateMaxPoints(7, 3));
+}
+
+@Test
+public void testGetQuestionById() {
+    assertNotNull(createQuestionInt.getQuestionById(1));
+    assertNull(createQuestionInt.getQuestionById(5));
+}
+
+@Test
+public void testAddQuestion() {
+    assertTrue(createQuestionInt.addQuestion("Question 1", "Option 1", "Option 2", 2, 10));
+    assertFalse(createQuestionInt.addQuestion(null, "Option 1", "Option 2", 2, 10));
+    assertFalse(createQuestionInt.addQuestion("Question 2", null, "Option 2", 2, 10));
+    assertFalse(createQuestionInt.addQuestion("Question 3", "Option 1", null, 2, 10));
+    assertFalse(createQuestionInt.addQuestion("Question 4", "Option 1", "Option 2", -1, 10));
+    assertFalse(createQuestionInt.addQuestion("Question 5", "Option 1", "Option 2", 2, -1));
+}
+
+@Test
+public void testDeleteQuestion() {
+    assertTrue(createQuestionInt.deleteQuestion(1));
+    assertFalse(createQuestionInt.deleteQuestion(5));
+}
 	
 }
