cf3db63f6981271474d0bea6caa65ac061916532
Mikel Lonbide <100244220+Mikeloon@users.noreply.github.com>
2023-05-31 11:39:12 +0200

    Update CreateQuestionDAW.java

--- src/test/java/CreateQuestionDAW.java
+++ src/test/java/CreateQuestionDAW.java
@@ -20,107 +20,39 @@ import test.dataAccess.TestDataAccess;
 
 public class CreateQuestionDAW {
 
-	 //sut:system under test
-	 static DataAccess sut=new DataAccess();
-	 
-	 //additional operations needed to execute the test 
-	 static TestDataAccess testDA=new TestDataAccess();
+	private CreateQuestionDAW createQuestionDAW;
 
-	private Event ev;
-	
-	@Test
-	//sut.createQuestion:  The event has one question with a queryText. 
-	public void test1() {
-		try {
-			
-			//define paramaters
-			String eventText="event1";
-			String queryText="query1";
-			Float betMinimum=new Float(2);
-			
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-			Date oneDate=null;;
-			try {
-				oneDate = sdf.parse("05/10/2022");
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}	
-			
-			//configure the state of the system (create object in the dabatase)
-			testDA.open();
-			ev = testDA.addEventWithQuestion(eventText,oneDate,queryText, betMinimum);
-			testDA.close();
-			
-			
-			//invoke System Under Test (sut)  
-			sut.createQuestion(ev, queryText, betMinimum);
-			
-			
-			//if the program continues fail
-		    fail();
-		   } catch (QuestionAlreadyExist e) {
-			// if the program goes to this point OK  
-			//fail();
-			   assertTrue(true);
-			} finally {
-				  //Remove the created objects in the database (cascade removing)   
-				testDA.open();
-		         boolean b=testDA.removeEvent(ev);
-		          testDA.close();
-		         System.out.println("Finally "+b);          
-		        }
-		   }
-	@Test
-	//sut.createQuestion:  The event has NOT one question with a queryText. 
-	public void test2() {
-		try {
-			
-			//define paramaters
-			String eventText="event1";
-			String queryText="query1";
-			Float betMinimum=new Float(2);
-			
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-			Date oneDate=null;;
-			try {
-				oneDate = sdf.parse("05/10/2022");
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}	
-			
-			//configure the state of the system (create object in the dabatase)
-			testDA.open();
-			ev = testDA.addEventWithQuestion(eventText,oneDate,"query2", betMinimum);
-			testDA.close();			
-			
-			//invoke System Under Test (sut)  
-			Question q=sut.createQuestion(ev, queryText, betMinimum);
-			
-			
-			//verify the results
-			assertTrue(q!=null);
-			assertEquals(q.getQuestion(),queryText);
-			assertEquals(q.getBetMinimum(),betMinimum,0);
-			
-			//q is DB
-			testDA.open();
-			boolean exist = testDA.existQuestion(ev,q);
-				
-			assertTrue(exist);
-			testDA.close();
-			
-		   } catch (QuestionAlreadyExist e) {
-			// TODO Auto-generated catch block
-			// if the program goes to this point fail  
-			fail();
-			} finally {
-				  //Remove the created objects in the database (cascade removing)   
-				testDA.open();
-		          boolean b=testDA.removeEvent(ev);
-		          testDA.close();
-		      //     System.out.println("Finally "+b);          
-		        }
-		   }
+@BeforeEach
+public void setUp() {
+    createQuestionDAW = new CreateQuestionDAW();
+}
+
+@Test
+public void testCalculateMaxPoints() {
+    assertEquals(10, createQuestionDAW.calculateMaxPoints(5, 2));
+    assertEquals(0, createQuestionDAW.calculateMaxPoints(0, 0));
+    assertEquals(15, createQuestionDAW.calculateMaxPoints(7, 3));
+}
+
+@Test
+public void testGetQuestionById() {
+    assertNotNull(createQuestionDAW.getQuestionById(1));
+    assertNull(createQuestionDAW.getQuestionById(5));
+}
+
+@Test
+public void testAddQuestion() {
+    assertTrue(createQuestionDAW.addQuestion("Question 1", "Option 1", "Option 2", 2, 10));
+    assertFalse(createQuestionDAW.addQuestion(null, "Option 1", "Option 2", 2, 10));
+    assertFalse(createQuestionDAW.addQuestion("Question 2", null, "Option 2", 2, 10));
+    assertFalse(createQuestionDAW.addQuestion("Question 3", "Option 1", null, 2, 10));
+    assertFalse(createQuestionDAW.addQuestion("Question 4", "Option 1", "Option 2", -1, 10));
+    assertFalse(createQuestionDAW.addQuestion("Question 5", "Option 1", "Option 2", 2, -1));
+}
+
+@Test
+public void testDeleteQuestion() {
+    assertTrue(createQuestionDAW.deleteQuestion(1));
+    assertFalse(createQuestionDAW.deleteQuestion(5));
+}
 }
