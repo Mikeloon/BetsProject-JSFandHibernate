ef0204f760f1497616e4128dbd5e593eed155175
Mikel Lonbide <100244220+Mikeloon@users.noreply.github.com>
2023-05-31 12:27:27 +0200

    Update TestFacadeImplementation.java

--- src/test/java/test/businessLogic/TestFacadeImplementation.java
+++ src/test/java/test/businessLogic/TestFacadeImplementation.java
@@ -6,8 +6,6 @@ import java.util.Date;
 import configuration.ConfigXML;
 import domain.Event;
 import test.dataAccess.TestDataAccess;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class TestFacadeImplementation {
 	TestDataAccess dbManagerTest;
@@ -37,33 +35,5 @@ public class TestFacadeImplementation {
 			return o;
 
 		}
-	
-	  // START - Additional tests
-
-    @Test
-    public void testRemoveEvent() {
-        Event event = new Event();
-        boolean result = removeEvent(event);
-        assertTrue(result);
-    }
-
-    @Test
-    public void testAddEventWithQuestion() {
-        String description = "Event description";
-        Date date = new Date();
-        String question = "Question";
-        float quantity = 10.0f;
-        
-        Event event = addEventWithQuestion(description, date, question, quantity);
-        assertNotNull(event);
-        assertEquals(description, event.getDescription());
-        assertEquals(date, event.getDate());
-        
-        // Check if the event has a question
-        assertNotNull(event.getQuestions());
-        assertFalse(event.getQuestions().isEmpty());
-        assertEquals(question, event.getQuestions().get(0).getQuestionText());
-        assertEquals(quantity, event.getQuestions().get(0).getBetMinimum(), 0.001);
-    }
 
 }
